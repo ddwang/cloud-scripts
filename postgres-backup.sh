d28b446ec66a76d9a79c18da78d37ed96e6de3c9
Daniel Wang <ddwang@gmail.com>
2022-07-03 17:41:48 -0700

    wip

---
+++ postgres-backup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+
+if [ -z "$1" ]
+  then
+	echo ""
+    echo "Usage: $0 <database_name>"
+	echo ""
+	exit 1
+fi
+
+db_name=$1
+date_dir="$(date +'%Y-%m-%d')"
+
+backup_dump="pg_dump_$date_dir.dump"
+
+cd /tmp
+sudo -u influxdb -H -- rm -rf $backup_dump
+
+echo ""
+echo ">>> Creating database dump at '$backup_dump'"
+sudo -u postgres -H -- pg_dump -F c $db_name > $backup_dump
+
+echo ""
+echo ">>>"
+ls -l $backup_dump
+echo ""
